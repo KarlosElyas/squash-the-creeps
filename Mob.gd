3c39f3d02154890c9b286612bfdc05f817b5960e
KarlosElyas <karloselyas2@gmail.com>
2023-07-25 15:00:22 -0300

    Instanciando inimigos

--- Mob.gd
+++ Mob.gd
@@ -9,15 +9,15 @@ func _physics_process(delta):
 func initialize(start_position, player_position):
 	look_at_from_position(start_position, player_position, Vector3.UP)
 	
-	rotate_y(randf_range(-PI / 4, PI / 4))
+	rotate_y(randf_range(-PI / 4, PI / 4)) #define a rotação
 	var random_speed = randi_range(min_speed, max_speed)
 	velocity = Vector3.FORWARD * random_speed
-	# ajustando a direção de acordo com o eixo Y do mob
+	# ajustando a DIREÇÃO de acordo com o EIXO Y do mob
 	velocity = velocity.rotated(Vector3.UP, rotation.y)
+	# neste caso o VECTOR3.UP respresenta o EIXO Y que é girado
 
 func _on_visible_on_screen_notifier_3d_screen_exited():
-	print_debug("destruido!")
-	queue_free()
+	queue_free() #quem sabe um dia isso funcione!
 
 func _on_autodestroi_timeout():
 	queue_free()
